8a7a1f301fc43a1298862ca4c03ceb53f5164b71
Jean-Yves Mengant <jymengant.teaser@gmail.com>
2024-04-14 15:43:25 +0200

    bug fixing and improvements

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # CodeMirror 6 language package tefor Abc music notation syntax
 
+This package implements Abc Music notation support for the
+[CodeMirror](https://codemirror.net/) code editor.
+
 This is a codemirror parser for Abc music notation syntax language
 
 - Build (`npm run prepare`) and test (`npm test`).
 
-- Publish. package on npm under `codemirror-lang-abcmusic`.
+# API Reference
+
+<dl>
+
+<dd><p>Completion source that looks up locally defined names in Abc music format.</p>
+
+</dd>
+<dt id="user-content-abcmusiclanguage">
+  <code><strong><a href="#user-content-abcmusiclanguage">AbcMusicLanguage</a></strong>: <a href="https://codemirror.net/docs/ref#language.LRLanguage">LRLanguage</a></code></dt>
+</dl>
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {styleTags, tags as t} from "@lezer/highlight"
 
 export const AbcMusicLanguage = LRLanguage.define({
   parser: parser.configure({
+    strict : false,
     props: [
       indentNodeProp.add({
         Application: delimitedIndent({closing: ")", align: false})
@@ -13,9 +14,32 @@ export const AbcMusicLanguage = LRLanguage.define({
       }),
       styleTags({
         Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
+        Composer: t.keyword,
+        Key: t.keyword,
+        Length: t.keyword,
+        Meter: t.keyword,
+        Notes: t.keyword,
+        Origin: t.keyword,
+        Tempo: t.keyword,
+        Rythm: t.keyword,
+        Source: t.keyword,
+        Book: t.keyword,
+        Discography: t.keyword,
+        File: t.keyword,
+        Title: t.keyword,
+        Transcription: t.keyword,
+        Voice: t.keyword,
+        Words: t.keyword,
+        Redefine: t.keyword,
+        Bar: t.bracket ,
+        Instructions: t.meta,
+        TermRepeat: t.bracket ,
+        ChordStart: t.bracket ,
+        ChordEnd: t.bracket ,
+        ReferenceNumber : t.bool ,
+        Informations: t.string,
         LineComment: t.lineComment,
+        NoteRythm: t.character,
         "( )": t.paren
       })
     ]
